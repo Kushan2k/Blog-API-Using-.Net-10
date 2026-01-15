221916384df4b0e19a3a0bbf75182b11f452aba8
kushan2k <kushangayantha001@gmail.com>
2026-01-15 11:02:39 +0530

    updated to json

--- Dtos/User/UserCreateDto.cs
+++ Dtos/User/UserCreateDto.cs
@@ -1,10 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace learn.Dtos.User;
 
 public record UserCreateDto(
+
+    [property:JsonPropertyName("email")]
     [Required][EmailAddress] string Email,
+    [property:JsonPropertyName("password")]
     [Required][MinLength(6)] string Password,
+    [property:JsonPropertyName("full_name")]
     [Required] string FullName,
+    [property:JsonPropertyName("confirm_password")]
     [Required] string ConfirmPassword
 );
