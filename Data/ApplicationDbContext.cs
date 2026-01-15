09b543ff0b64127bc60bf1a558f6c3fa7341de98
kushan2k <kushangayantha001@gmail.com>
2026-01-15 11:02:29 +0530

    email unique constraint added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,4 +9,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
     public DbSet<Blog> Blogs { get; set; }
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
 }
