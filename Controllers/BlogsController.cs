7c9045fcbceebdf7391c34f0e98b15c2347a6b8c
kushan2k <kushangayantha001@gmail.com>
2026-01-17 00:40:31 +0530

    blog create dto added

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -1,27 +1,41 @@
 using System.Net;
+using learn.Dtos.Blog;
 using learn.Services.BlogService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace learn.Controllers;
 
 [ApiController]
 [Route("api/blogs")]
-public class BlogsController : ControllerBase
+public class BlogsController(IBlogService blogService) : ControllerBase
 {
 
-    private readonly IBlogService _blogService;
+    private readonly IBlogService _blogService = blogService;
 
-    public BlogsController(IBlogService blogService)
-    {
-        _blogService = blogService;
-    }
-
-    [HttpGet]
+  [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult> GetAllBlogs()
     {
 
 
         return Ok(new { message = "All Blogs fetched successfully", data = await _blogService.GetAllBlogsAsync(), status = HttpStatusCode.OK });
+    }
+
+    [HttpGet("/{id}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetBlogById(int id)
+    {
+
+        return Ok(new { message = "Blog fetched successfully", status = HttpStatusCode.OK });
+    }
+
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogCreateDto blogCreateDto)
+    {
+        return Ok(new { message = "Blog created successfully", status = HttpStatusCode.OK });
     }
 
 }
