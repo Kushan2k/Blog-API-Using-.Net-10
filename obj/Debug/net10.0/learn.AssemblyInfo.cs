c12fabef36ec22a25afefe597f9d89d5b7ed611c
Kushan2k <kushangayantha001@gmail.com>
2026-01-17 19:11:21 +0530

    changes

--- obj/Debug/net10.0/learn.AssemblyInfo.cs
+++ obj/Debug/net10.0/learn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4433bf2c6042bf52cd63910215cfd70944e1185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1405c28c3dce85b04ef0a39bd189b9532f37d582")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
