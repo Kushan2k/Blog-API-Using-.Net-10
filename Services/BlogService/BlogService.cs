fb64fcbb1503be095902d1f1768d0b1ba46b9a93
Kushan2k <kushangayantha001@gmail.com>
2026-01-17 22:44:50 +0530

    get by id/created services added

--- Services/BlogService/BlogService.cs
+++ Services/BlogService/BlogService.cs
@@ -1,20 +1,71 @@
 using learn.Data;
+using learn.Dtos.Blog;
 using learn.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ILogger = learn.Factories.Logger.ILogger;
 
 namespace learn.Services.BlogService;
 
 
-public class BlogService(ApplicationDbContext _context) : IBlogService
+public class BlogService(ApplicationDbContext _context,IHttpContextAccessor _httpContextAccessor,ILogger _logger) : IBlogService
 {
-    public void CreatePost(string title, string content)
+  public async Task<IActionResult> CreateBlogAsync(BlogCreateDto blogCreateDto)
     {
-        throw new NotImplementedException();
-    }
+    
+        try
+        {
+            var userId= _httpContextAccessor.HttpContext?.Items["UserId"];
+            var blog = new Blog
+            {
+                Title = blogCreateDto.Title,
+                Content = blogCreateDto.Content,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                AuthorId = Convert.ToInt32(userId)
+            };
+        
+            _context.Blogs.Add(blog);
+            await _context.SaveChangesAsync();
 
-    public async Task<ICollection<Blog>> GetAllBlogsAsync()
+            
+        
+            return new CreatedAtRouteResult(
+                "GetBlogById",
+                new { id = blog.Id },
+
+                new { message = "Blog created successfully", status = System.Net.HttpStatusCode.Created, blog = new BlogDto(blog.Id, blog.Title, blog.Content, blog.CreatedAt, blog.UpdatedAt) });
+        }
+        catch (System.Exception ex)
+        {
+            _logger.LogError($"[{DateTime.Now}] -- Error creating blog: {ex.Message}");
+            return new StatusCodeResult(500);
+        }
+  }
+
+  public async Task<ICollection<Blog>> GetAllBlogsAsync()
     {
         var blogs = await _context.Blogs.ToListAsync();
         return blogs;
+    }
+
+  public Task<IActionResult> GetBlogByIdAsync(int id)
+  {
+    try
+    {
+        var blog =  _context.Blogs.FirstOrDefault(b => b.Id == id);
+        if (blog == null)
+        {
+            return Task.FromResult<IActionResult>(new NotFoundObjectResult(new { message = "Blog not found", status = System.Net.HttpStatusCode.NotFound }));
+        }
+
+        return Task.FromResult<IActionResult>(new OkObjectResult(new { message = "Blog fetched successfully", status = System.Net.HttpStatusCode.OK, blog = new BlogDto(blog.Id, blog.Title, blog.Content, blog.CreatedAt, blog.UpdatedAt) }));
+    }
+    catch (System.Exception ex)
+        {
+            
+            _logger.LogError($"[{DateTime.Now}] -- Error fetching blog by id: {ex.Message}");
+            return Task.FromResult<IActionResult>(new StatusCodeResult(500));
+        }
     }
 }
