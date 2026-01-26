be89753d3e0b3030adc3e803c0ab452e81030bd6
Kushan2k <kushangayantha001@gmail.com>
2026-01-26 22:23:29 +0530

    logger for middler ware added

--- Middlewares/UserMiddleware.cs
+++ Middlewares/UserMiddleware.cs
@@ -1,13 +1,17 @@
 namespace learn.Middlewares;
 
+using ILogger = learn.Factories.Logger.ILogger;
+
 
 public class UserMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILogger _logger;
 
-    public UserMiddleware(RequestDelegate next)
+    public UserMiddleware(RequestDelegate next, ILogger logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -15,9 +19,17 @@ public class UserMiddleware
         // Middleware logic can be added here
         if (context.User.Identity?.IsAuthenticated == true)
         {
-            var userid = context.User.FindFirst("userId")?.Value;
+            try
+            {
+                var userid = context.User.FindFirst("userId")?.Value;
 
-            context.Items["UserId"] = userid;
+                context.Items["UserId"] = userid;
+            }
+            catch (System.Exception)
+            {
+                _logger.LogWarning("UserMiddleware: Unable to extract userId from token.");
+                await _next(context);
+            }
         }
 
         await _next(context);
